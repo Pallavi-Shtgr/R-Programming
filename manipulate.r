4dc02434ae6ded07e7e295c3e907f558dc98f5d2
Pallavi <126679884+Pallavi-Shtgr@users.noreply.github.com>
2024-06-30 08:08:06 +0530

    Update manipulate.r

--- manipulate.r
+++ manipulate.r
@@ -1,1 +1,11 @@
+#manipulating data using ends with 
+starwars %>% 
+  select(name,height,ends_with("color")) %>% 
+  names()
 
+
+#manipulating using filter
+starwars %>% 
+  select(name,height,ends_with("color")) %>% 
+  filter(hair_color %in% c("blond","brown") & height<180) %>% 
+  view()
